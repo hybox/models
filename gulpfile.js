f3c78f46382e99802d99721be38083f26a1309cd
Mark A. Matienzo <matienzo@stanford.edu>
2016-12-13 23:12:31 -0500

    Cleanup gulp tasks and add ebook builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');  
 const del = require('del');
 const gulpGitbook = require('gulp-gitbook');  
+const shell = require('gulp-shell');
 const gulpLoadPlugins = require('gulp-load-plugins');
 
 const $ = gulpLoadPlugins();
@@ -21,7 +22,18 @@ gulp.task('clean', () => {
 });
 
 gulp.task('build', function (cb) {
+  console.log('Building HTML version')
   gulpGitbook('.', cb);
+});
+
+// FIXME: smh, you should actually learn how to use gulp
+gulp.task('build-ebooks', () => {
+  console.log('Building PDF, ePUB, and MOBI versions')
+  shell.task([
+    'gitbook pdf docs _book/hybox-models.pdf',
+    'gitbook epub docs _book/hybox-models.epub',
+    'gitbook mobi docs _book/hybox-models.mobi',
+  ]);
 });
 
 gulp.task('default', ['clean', 'build', 'publish']);
